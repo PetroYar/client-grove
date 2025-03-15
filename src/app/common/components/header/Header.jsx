6f42097e99635b20a852726236407c5d58add5c0
PetroYar <petryarish@gmail.com>
2025-03-15 19:27:01 +0200

    petro
    overflov

--- src/app/common/components/header/Header.jsx
+++ src/app/common/components/header/Header.jsx
@@ -13,14 +13,14 @@ const Header = (props) => {
   const toggleBurger = () => {
     setShowBurger((prev) => !prev);
     if (!showBurger) {
-      document.body.style.overflow = "hidden";
+      document.body.style.overflowY = "hidden";
     } else {
-      document.body.style.overflow = "auto";
+      document.body.style.overflowY = "auto";
     }
   };
   const handleLinkClick = () => {
     setShowBurger(false); 
-    document.body.style.overflow = "auto"; 
+    document.body.style.overflowY = "auto"; 
   };
   return (
     <header className={styles.header}>
